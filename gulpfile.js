9a22e2049ce0aa94e445735bdfabb96c086780db
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2015-11-10 12:29:21 +0100

    exclude Sass files from lint
    Exclude from lint all files written in Sass

--- gulpfile.js
+++ gulpfile.js
@@ -110,7 +110,7 @@ gulp.task('styles:fabricator', function () {
 });
 
 gulp.task("lint-styles", function() {
-    return gulp.src("src/assets/toolkit/styles/*.css")
+    return gulp.src(["src/assets/toolkit/styles/*.css", "!src/assets/toolkit/styles/__*.css"])
     .pipe(postcss([
         stylelint(stylelintConfig),
         reporter({
