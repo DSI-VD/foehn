8f73889f7db1a8a7961e0c6b4659ce624e7dc384
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2016-06-30 15:18:43 +0200

    externalize gulp configuration

--- gulp-task/lint-scripts.js
+++ gulp-task/lint-scripts.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var gulp = require('gulp'),
-    eslint = require('gulp-eslint');
+    eslint = require('gulp-eslint'),
+    config = require('../gulp-config.json');
 
 module.exports = function() {
     gulp.task('lint-scripts', function () {
-        return gulp.src(['src/assets/foehn/**/*.js', './gulpfile.js', './gulp-task/**/*.js', '!node_modules/**'])
+        return gulp.src(['src/assets/foehn/**/*.js', './gulpfile.js', config.tasks + '**/*.js', '!node_modules/**'])
             .pipe(eslint())
             .pipe(eslint.format())
             .pipe(eslint.failAfterError());
