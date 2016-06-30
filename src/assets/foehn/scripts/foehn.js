22f9d54c6e23a631cd6622ff27ea7d7b11f0273f
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2016-06-30 13:40:04 +0200

    set js linting rules

--- src/assets/foehn/scripts/foehn.js
+++ src/assets/foehn/scripts/foehn.js
@@ -11,6 +11,6 @@ var WebFont = require('webfontloader');
 
 WebFont.load({
     custom: {
-      families: ['MetaSerifWeb:n4,i4,n7,i7', 'MetaWeb:n4,i4,n7,i7']
+        families: ['MetaSerifWeb:n4,i4,n7,i7', 'MetaWeb:n4,i4,n7,i7']
     }
 });
