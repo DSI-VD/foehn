116332cc2aeb09b67dc234cff46b9364ed7cd0ac
Toni Fisler <mail@tonifisler.ch>
2016-10-18 10:56:56 +0200

    fix some toolbox issues

--- tasks/metalsmith.js
+++ tasks/metalsmith.js
@@ -12,6 +12,7 @@ import path from 'path';
 import markdown from 'metalsmith-markdown';
 import permalinks from 'metalsmith-permalinks';
 import layouts from 'metalsmith-layouts';
+import inplace from 'metalsmith-in-place';
 import define from 'metalsmith-define';
 import collections from 'metalsmith-collections';
 
@@ -78,6 +79,7 @@ export const metalsmithDocs = () => {
     .pipe($.plumber({errorHandler: errorAlert}))
     .pipe($.metalsmith({
       use: [
+        inplace(config.metalsmith.plugins.metalsmithinplace),
         markdown({ langPrefix: 'language-' }),
         collections(config.metalsmith.plugins.collections),
         function(files, metalsmith, done){
@@ -99,6 +101,13 @@ export const metalsmithDocs = () => {
           data: metadatas
         }),
         layouts(config.metalsmith.plugins.layouts),
+        function(files, metalsmith, done){
+          // Clean dirty front-matter comment
+          for (let file in files) {
+            files[file].contents = new Buffer(files[file].contents.toString().replace(/---[\s\S]*?---/g, ''));
+          }
+          done();
+        },
         permalinks(config.metalsmith.plugins.permalinks)
       ]
     }))
