9c6a612afd44287222facf0ac31ed253cde754af
Cédric Aellen <cedric.aellen@vd.ch>
2016-12-23 14:50:32 +0100

    add fractal.js

---
+++ fractal.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* Create a new Fractal instance and export it for use elsewhere if required */
+const fractal = module.exports = require('@frctl/fractal').create();
+
+/* Set the title of the project */
+fractal.set('project.title', 'Foehn');
+
+/* Tell Fractal where the components will live */
+fractal.components.set('path', __dirname + '/src/components');
+
+/* Tell Fractal where the documentation pages will live */
+fractal.docs.set('path', __dirname + '/src/docs');
