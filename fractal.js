e8d83167ded47d557ff02f60feb9b28e821865b8
Cédric Aellen <cedric.aellen@vd.ch>
2016-12-27 15:28:27 +0100

    set the styles in preview

--- fractal.js
+++ fractal.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const paths = {
+  build: __dirname + '/www',
+  src: __dirname + '/src',
+  static: __dirname + '/tmp'
+};
+
+const mandelbrot = require('@frctl/mandelbrot')({
+  lang: 'en',
+  skin: 'green'
+});
+
 /* Create a new Fractal instance and export it for use elsewhere if required */
 const fractal = module.exports = require('@frctl/fractal').create();
 
@@ -7,7 +18,22 @@ const fractal = module.exports = require('@frctl/fractal').create();
 fractal.set('project.title', 'Foehn');
 
 /* Tell Fractal where the components will live */
-fractal.components.set('path', __dirname + '/src/components');
+fractal.components.set('path', paths.src + '/components');
 
 /* Tell Fractal where the documentation pages will live */
-fractal.docs.set('path', __dirname + '/src/docs');
+fractal.docs.set('path', paths.src + '/docs');
+
+/* Destination for the static export */
+fractal.web.set('builder.dest', paths.build);
+
+/* Tell Fractal where static assets are */
+fractal.web.set('static.path', paths.static);
+
+/* Tell fractal to not use file extension when rewriting URLs */
+fractal.web.set('builder.urls.ext', null);
+
+/* Tell fractal wich theme to use */
+fractal.web.theme(mandelbrot);
+
+/* Tell Fractal what is the default preview */
+fractal.components.set('default.preview', '@preview')
