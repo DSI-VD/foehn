0d11618b0f9068bef169c2789f0a38e5bdc8bd3f
Cédric Aellen <cedric.aellen@vd.ch>
2017-01-18 15:17:41 +0100

    use faker to generate list

---
+++ src/components/03-organisms/example-list.config.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const faker = require('faker'); // require the faker module
+const peopleCount = 10; // how many peoples we should generate data for
+const peopleData = [];
+
+// sets locale to fr
+faker.locale = "fr";
+
+for (var i = 0; i < peopleCount; i++) {
+   peopleData.push({
+       name: faker.name.findName(), // generate a random name
+       email: faker.internet.email(),  // generate a random email address
+       jobTitle: faker.name.jobTitle(),  // generate a random email address
+       jobType: faker.name.jobTitle(),  // generate a random email address
+       streetPrefix: faker.address.streetPrefix(),  // generate a random email address
+       street: faker.address.streetName(),  // generate a random email address
+       city: faker.address.city()  // generate a random email address
+   });
+}
+
+module.exports = {
+   context: {
+       peoples: peopleData // use our generated list of peoples as context data for our template.
+   }
+};
