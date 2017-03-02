263e0dab32e6ed6640b9fb4f15f7a457ca78575d
Cédric Aellen <cedric.aellen@vd.ch>
2017-03-02 16:22:13 +0100

    refactor

--- fractal.js
+++ fractal.js
@@ -14,35 +14,27 @@ const mandelbrot = require('@frctl/mandelbrot')({
     skin: 'navy',
 });
 
-/* Create a new Fractal instance and export it for use elsewhere if required */
+/* Create a new Fractal */
 const fractal = require('@frctl/fractal').create();
 
-/* Set the title of the project */
-fractal.set('project.title', 'Foehn');
+// Project config
+fractal.set('project.title', 'Foehn'); // set the title of the project
 
 // Components config
 fractal.components.set('default.status', 'wip');
 fractal.components.engine('@frctl/nunjucks'); // register the Nunjucks adapter for your components
 fractal.components.set('ext', '.html'); // look for files with a .nunj file extension
 fractal.components.set('path', `${paths.src}/components`); // tell Fractal where the components will live
+fractal.components.set('default.preview', '@preview'); // Tell Fractal what is the default preview
 
-/* Tell Fractal where the documentation pages will live */
-fractal.docs.set('path', `${paths.src}/docs`);
+// Docs config
+fractal.docs.set('path', `${paths.src}/docs`); // tell Fractal where the documentation pages will live
 
-/* Destination for the static export */
-fractal.web.set('builder.dest', paths.build);
-
-/* Tell Fractal where static assets are */
-fractal.web.set('static.path', paths.static);
-
-/* Tell fractal to not use file extension when rewriting URLs */
-fractal.web.set('builder.urls.ext', null);
-
-/* Tell fractal wich theme to use */
-fractal.web.theme(mandelbrot);
-
-/* Tell Fractal what is the default preview */
-fractal.components.set('default.preview', '@preview');
+// Web UI config
+fractal.web.theme(mandelbrot); // Tell fractal wich theme to use
+fractal.web.set('builder.dest', paths.build); // destination for the static export
+fractal.web.set('static.path', paths.static); // tell Fractal where static assets are
+fractal.web.set('builder.urls.ext', null); // tell fractal to not use file extension when rewriting URLs
 
 // Export config
 module.exports = fractal;
