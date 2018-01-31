a9acbcdd6e2ff8a736b03f127f5c3a625acd969e
Cédric Aellen <cedric.aellen@vd.ch>
2018-01-31 10:16:13 +0100

    Update 2018-01-31T09:16:02.142Z

---
+++ assets/scripts/foehn-scripts--footer.js
@@ -0,0 +1,14 @@
+/**
+ * Close nav-primary if the page is loaded on a mobile phone
+ */
+$(document).ready(function closePrimaryNav() {
+    const screen = $(window);
+    if (screen.width() <= 576) {
+        // set the button as closed
+        $('.js-closeMenuOnMobile__button').addClass('collapsed').attr('aria-expanded', 'false');
+        // set the menu as closed
+        $('.js-closeMenuOnMobile__container').removeClass('show');
+    }
+});
+
+//# sourceMappingURL=foehn-scripts--footer.js.map
