536b46e7e5ce3d28077d13ae711cf9e83c106704
Cédric Aellen <cedric.aellen@vd.ch>
2018-07-11 07:50:37 +0200

    Update 2018-07-11T05:47:11.383Z

--- assets/scripts/foehn-scripts--footer.js
+++
@@ -1,2 +0,0 @@
-$(document).ready(function(){$(window).width()<=576&&($(".js-closeMenuOnMobile__button").addClass("collapsed").attr("aria-expanded","false"),$(".js-closeMenuOnMobile__container").removeClass("show"))});
-//# sourceMappingURL=foehn-scripts--footer.js.map
