1c995d40f8348973fa114cf2e0a41706f1b02a2d
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2018-07-12 11:52:01 +0200

    Update 2018-07-12T09:51:49.830Z

---
+++ assets/scripts/foehn-scripts--footer.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$(window).width()<=576&&($(".js-closeMenuOnMobile__button").addClass("collapsed").attr("aria-expanded","false"),$(".js-closeMenuOnMobile__container").removeClass("show"))});
+//# sourceMappingURL=foehn-scripts--footer.js.map
