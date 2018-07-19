7aedb3743ad98c77df41a4d4ea2840f971f68eab
Cédric Aellen <cedric.aellen@vd.ch>
2018-07-19 09:37:48 +0200

    Update 2018-07-19T07:37:39.473Z

--- assets/scripts/foehn-scripts--footer.js
+++ assets/scripts/foehn-scripts--footer.js
@@ -1,2 +1,2 @@
-$(document).ready(function(){$(window).width()<=576&&($(".js-closeMenuOnMobile__button").addClass("collapsed").attr("aria-expanded","false"),$(".js-closeMenuOnMobile__container").removeClass("show"))});
+$(document).ready(()=>{$(window).width()<=576&&($(".js-closeMenuOnMobile__button").addClass("collapsed").attr("aria-expanded","false"),$(".js-closeMenuOnMobile__container").removeClass("show"))});
 //# sourceMappingURL=foehn-scripts--footer.js.map
