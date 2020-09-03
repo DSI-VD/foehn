8dfe9eb4f74503f06904849818ff32c4eda6d3d3
Cédric Aellen <cedric.aellen@vd.ch>
2020-09-03 16:07:53 +0200

    remove fractal

--- gulpfile.js
+++ gulpfile.js
@@ -46,53 +46,6 @@ const banner = ['/**',
     ' */',
     ''].join('\n');
 
-/*
- * Configure a Fractal instance.
- */
-
-const fractal = require('./fractal.js');
-
-const logger = fractal.cli.console; // Keep a reference to the fractal CLI console utility
-
-/*
- * Start the Fractal server
- *
- * In this example we are passing the option 'sync: true' which means that it will
- * use BrowserSync to watch for changes to the filesystem and refresh the browser automatically.
- * Obviously this is completely optional!
- *
- * This task will also log any errors to the console.
- */
-function serve() {
-    const server = fractal.web.server({
-        sync: true
-    });
-
-    server.on('error', err => logger.error(err.message));
-
-    return server.start().then(() => {
-        logger.success(`Fractal server is now running at ${server.url}`);
-    });
-}
-
-/*
- * Run a static export of the project web UI.
- *
- * This task will report on progress using the 'progress' event emitted by the
- * builder instance, and log any errors to the terminal.
- *
- * The build destination will be the directory specified in the 'builder.dest'
- * configuration option set above.
- */
-function build() {
-    const builder = fractal.web.builder();
-
-    builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
-    builder.on('error', err => logger.error(err.message));
-    return builder.build().then(() => {
-        logger.success('Fractal build completed!');
-    });
-}
 
 /*
  * Clean Dest
@@ -330,6 +283,6 @@ const compile = gulp.series(
     lintjson
 );
 
-gulp.task('build', gulp.series(clean, compile, build));
+gulp.task('build', gulp.series(clean, compile));
 gulp.task('dev', gulp.series(cleanDest, compile, watch));
-gulp.task('publish', gulp.series(build, deploy));
+gulp.task('publish', gulp.series(deploy));
