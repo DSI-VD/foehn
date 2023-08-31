5d56e3c708b35b322d50211f8b2037c639bb1654
Steven Avelino <steven.avelino@vd.ch>
2023-08-31 07:55:36 +0200

    [FIX][CCWEB-15226] Make the collapse container closeable by clicking outside of it

--- src/assets/scripts/collapses.js
+++ src/assets/scripts/collapses.js
@@ -2,8 +2,22 @@
  * Code related to collapses
  */
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#login-button').addEventListener('click', () => {
+    const loginCollapse = new bootstrap.Collapse('#login-form-collapse', {
+        toggle: false
+    });
+    document.addEventListener('click', event => {
+        if (!document.querySelector('#login-form-collapse').contains(event.target)) {
+            loginCollapse.hide();
+        }
+    });
+
+    document.querySelector('#login-form-collapse').addEventListener('hide.bs.collapse', () => {
         document.querySelector('#login-button-icon').classList.toggle('fa-chevron-down');
         document.querySelector('#login-button-icon').classList.toggle('fa-chevron-up');
-    });
+    })
+
+    document.querySelector('#login-form-collapse').addEventListener('show.bs.collapse', () => {
+        document.querySelector('#login-button-icon').classList.toggle('fa-chevron-down');
+        document.querySelector('#login-button-icon').classList.toggle('fa-chevron-up');
+    })
 });
