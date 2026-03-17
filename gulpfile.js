00361b353ba7bb1667f8803baf5a33249c34b294
Nogueira Ludovic <nogueira.ludovic@gmail.com>
2026-03-17 14:02:26 +0100

    [TASK][VDCH-20381] Adapt the gulpfile and some css

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,44 @@
-'use strict';
-
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {readFileSync} from 'node:fs';
 import gulp from 'gulp';
 import postcss from 'gulp-postcss';
 import concat from 'gulp-concat';
-import gulpSass from 'gulp-sass';
-import * as nodeSass from 'sass';
-import stylelint from 'gulp-stylelint';
-import xo from 'gulp-xo';
-import jsonlint from 'gulp-jsonlint';
-import path from 'path';
-import {deleteSync} from 'del';
-import {fileURLToPath} from 'url';
+import scss from 'gulp-dart-scss';
 import sourcemaps from 'gulp-sourcemaps';
 import uglifyjs from 'uglify-es';
 import composer from 'gulp-uglify/composer.js';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import header from 'gulp-header';
-import {readFileSync} from 'fs';
+import {deleteSync} from 'del';
+
 const pkg = JSON.parse(readFileSync('./package.json'));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const sass = gulpSass(nodeSass);
+
 const minify = composer(uglifyjs, console);
 const processors = [autoprefixer, cssnano];
+
 const paths = {
     build: path.join(__dirname, '/dist'),
-    src: path.join(__dirname, '/src')
+    src: path.join(__dirname, '/src'),
 };
+
 const banner = [
     '/**',
     ' * <%= pkg.name %> - <%= pkg.description %>',
     ' * @version v<%= pkg.version %>',
     ' */',
-    ''
+    '',
 ].join('\n');
 
 /*
  * Clean
  */
 function clean() {
-    const deleted = deleteSync([`${paths.build}`]);
-    return Promise.resolve(deleted);
+    deleteSync([paths.build]);
+    return Promise.resolve();
 }
 
 /*
@@ -49,37 +46,16 @@ function clean() {
  */
 function styles() {
     return gulp
-        .src([
-            `${paths.src}/assets/styles/main.scss`
-        ])
+        .src([`${paths.src}/assets/styles/main.scss`])
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
+        .pipe(scss({
+            quietDeps: true,
+            silenceDeprecations: ['color-functions', 'global-builtin', 'if-function', 'import', 'legacy-js-api'],
+        }))
         .pipe(postcss(processors))
         .pipe(header(banner, {pkg}))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(`${paths.build}/assets/styles`));
-}
-
-/*
- * Style linting
- */
-function lintstyles() {
-    return gulp
-        .src([
-            `${paths.src}/assets/styles/**/*.s+(a|c)ss`,
-            `!${paths.src}/assets/styles/_bs-variables.scss`
-        ])
-        .pipe(
-            stylelint({
-                failAfterError: false,
-                reporters: [
-                    {
-                        formatter: 'string',
-                        console: true
-                    }
-                ]
-            })
-        );
 }
 
 /*
@@ -90,15 +66,13 @@ function scriptsVendors() {
         .src([
             'node_modules/jquery/dist/jquery.slim.min.*',
             'node_modules/popper.js/dist/umd/popper.min.*',
-            'node_modules/bootstrap/dist/js/bootstrap.min.*'
+            'node_modules/bootstrap/dist/js/bootstrap.min.*',
         ])
         .pipe(gulp.dest(`${paths.build}/assets/scripts/`));
 }
 
 /*
  * Scripts footer
- *
- * the scripts that are placed in the footer of the document
  */
 function scriptsFooter() {
     return gulp
@@ -112,9 +86,6 @@ function scriptsFooter() {
 
 /*
  * SVG
- *
- * Make sure to minify the original SVGs
- * We do not minify during the build to speed up the process
  */
 function svg() {
     return gulp
@@ -123,10 +94,7 @@ function svg() {
 }
 
 /*
- * Image
- *
- * Make sure to minify the original images
- * We do not minify during the build to speed up the process
+ * Images
  */
 function images() {
     return gulp
@@ -162,42 +130,11 @@ function xmls() {
 }
 
 /*
- * Lint Scripts
- */
-function lintscripts() {
-    return gulp
-        .src([`${paths.src}/**/*.js`, './*.js', '!node_modules/**'])
-        .pipe(xo())
-        .pipe(xo.format());
-}
-
-/*
- * Lint JSON
- *
- * 1. Lint CSS source maps once they are generated. Be sure to run this function
- *    once source maps files are generated
- * 2. Lint JS source maps once they are generated. Be sure to run this function
- *    once source maps files are generated
- */
-function lintjson() {
-    return gulp
-        .src([
-            '*.json',
-            `${paths.src}/**/*.json`,
-            `${paths.build}/**/*.css.map`, // [1]
-            `${paths.build}/**/*.js.map` // [2]
-        ])
-        .pipe(jsonlint())
-        .pipe(jsonlint.reporter());
-}
-
-/*
  * Task set
  */
 const compile = gulp.series(
     gulp.parallel(
         styles,
-        lintstyles,
         scriptsVendors,
         scriptsFooter,
         svg,
@@ -205,9 +142,8 @@ const compile = gulp.series(
         fonts,
         manifests,
         xmls,
-        lintscripts
-    ),
-    lintjson
+    )
 );
+
 gulp.task('build', gulp.series(clean, compile));
 gulp.task('dev', gulp.series(compile));
