b719b4b7f0a9eba5bb4d8b9b3e09108eb9ce7d6f
Eric Chavaillaz <eric.chavaillaz@vd.ch>
2026-09-10 08:04:49 +0200

    [TASK][VDCH-21278] Fix Jenkins job

--- jenkins/Release.groovy
+++ jenkins/Release.groovy
@@ -1,4 +1,4 @@
-@Library('pee-library') _
+@Library('devsup-library') _
 
 try {
     def gitVersion = params.GIT_VERSION
@@ -6,8 +6,10 @@ try {
     def githubRepo = params.GITHUB_REPO
     def sourceBranch = params.SOURCE_BRANCH
     def npmVersion = params.NPM_VERSION
+    def npmRegistry = 'https://registry.npmjs.org/'
 
-    println "Releasing Foehn ${gitVersion}, nodeVersion=${nodeVersion}"
+    println "Releasing Foehn ${gitVersion} from branch ${sourceBranch}. " +
+        "nodeVersion=${nodeVersion}, npmVersion=${npmVersion}"
 
     if (!gitVersion) {
         throw new Exception('Missing build parameter "GIT_VERSION"')
@@ -25,64 +27,174 @@ try {
         throw new Exception('Missing build parameter "SOURCE_BRANCH"')
     }
 
-    def buildDir = "release-job"
-    def branchName = sourceBranch.replace("origin/", "")
+    if (!npmVersion) {
+        throw new Exception('Missing build parameter "NPM_VERSION"')
+    }
 
-    node('chromium-headless') {
+    def buildDir = 'release-job'
+    def branchName = sourceBranch.replace('origin/', '')
+
+    node('pee') {
         sh "rm -Rf ${buildDir} ; mkdir -p ${buildDir}"
 
         dir(buildDir) {
-            stage("[Foehn] : Clone repository") {
-              echo "Cloning the repository with branch ${branchName}"
-              checkout scm
-              gitCheckoutBranch(branchName)
+            stage('[Foehn] : Clone repository') {
+                echo "Getting branch ${branchName} from Git"
+
+                checkout scm
+                gitCheckoutBranch(branchName)
+                runGitCommand('fetch origin --tags --force')
             }
 
-            stage("[Foehn] : Install project") {
-              echo "Install project"
-              runYarnCommand("install", nodeVersion)
+            stage('[Foehn] : Validate release') {
+                sh """
+                    set -e
+
+                    PACKAGE_VERSION=\$(node -p "require('./package.json').version")
+                    EXPECTED_VERSION=\$(printf '%s' '${gitVersion}' | sed 's/^v//')
+
+                    echo "package.json version: \$PACKAGE_VERSION"
+                    echo "Git release version:  \$EXPECTED_VERSION"
+
+                    if [ "\$PACKAGE_VERSION" != "\$EXPECTED_VERSION" ]; then
+                        echo "ERROR: package.json version does not match GIT_VERSION"
+                        exit 1
+                    fi
+
+                    if ! git rev-parse '${gitVersion}^{commit}' >/dev/null 2>&1; then
+                        echo "ERROR: Git tag ${gitVersion} does not exist"
+                        exit 1
+                    fi
+
+                    TAG_COMMIT=\$(git rev-parse '${gitVersion}^{}')
+                    HEAD_COMMIT=\$(git rev-parse HEAD)
+
+                    echo "HEAD commit: \$HEAD_COMMIT"
+                    echo "Tag commit:  \$TAG_COMMIT"
+
+                    if [ "\$TAG_COMMIT" != "\$HEAD_COMMIT" ]; then
+                        echo "ERROR: ${gitVersion} does not point to current HEAD"
+                        exit 1
+                    fi
+                """
             }
 
-            stage("[Foehn] : Pushing to Github") {
-              echo "Push to Github"
-              runGitCommand("remote add Github ${githubRepo}")
-              runGitCommand("push Github ${branchName}")
+            stage('[Foehn] : Install project') {
+                runYarnCommand('install --frozen-lockfile', nodeVersion)
             }
 
-            stage("[Foehn] : Push new version to Github") {
-              runGitCommand("push Github ${gitVersion}")
+            stage('[Foehn] : Build project') {
+                runYarnCommand('build', nodeVersion)
             }
 
-            stage("[Foehn] : Deploy to npmjs") {
-              runNpmCommand("config set registry https://registry.npmjs.org/", nodeVersion)
-              runNpmCommand("publish --tag ${npmVersion}", nodeVersion)
+            stage('[Foehn] : Push branch to GitHub') {
+                sh """
+                    if git remote get-url Github >/dev/null 2>&1; then
+                        git remote set-url Github '${githubRepo}'
+                    else
+                        git remote add Github '${githubRepo}'
+                    fi
+                """
+
+                runGitCommand("fetch Github ${branchName}")
+                runGitCommand("push --force-with-lease Github ${branchName}:${branchName}")
+            }
+
+            stage('[Foehn] : Push tag to GitHub') {
+                sh """
+                    set -e
+
+                    LOCAL_COMMIT=\$(git rev-parse '${gitVersion}^{}')
+                    REMOTE_TAG=\$(git ls-remote --tags Github 'refs/tags/${gitVersion}' | cut -f1)
+                    REMOTE_PEELED=\$(git ls-remote --tags Github 'refs/tags/${gitVersion}^{}' | cut -f1)
+
+                    if [ -z "\$REMOTE_TAG" ]; then
+                        echo "Tag ${gitVersion} does not exist on GitHub"
+                        git push Github '${gitVersion}'
+                    else
+                        if [ -n "\$REMOTE_PEELED" ]; then
+                            REMOTE_COMMIT="\$REMOTE_PEELED"
+                        else
+                            REMOTE_COMMIT="\$REMOTE_TAG"
+                        fi
+
+                        echo "Local tag commit:  \$LOCAL_COMMIT"
+                        echo "Remote tag commit: \$REMOTE_COMMIT"
+
+                        if [ "\$LOCAL_COMMIT" = "\$REMOTE_COMMIT" ]; then
+                            echo "Tag ${gitVersion} already points to the correct commit on GitHub"
+                        else
+                            echo "ERROR: Tag ${gitVersion} exists on GitHub but points to another commit"
+                            exit 1
+                        fi
+                    fi
+                """
+            }
+
+            stage('[Foehn] : Deploy to npmjs') {
+                runNpmCommand(
+                    "whoami --registry=${npmRegistry}",
+                    nodeVersion
+                )
+
+                def packageVersion = runNodeCommand(
+                    "node -p \"require('./package.json').version\"",
+                    nodeVersion
+                )
+
+                def publishedVersion = runNodeCommand(
+                    "npm view @dsivd/foehn@${packageVersion} version " +
+                        "--registry=${npmRegistry} 2>/dev/null || true",
+                    nodeVersion
+                )
+
+                if (publishedVersion == packageVersion) {
+                    echo "@dsivd/foehn@${packageVersion} is already published. Skipping npm publish."
+                } else {
+                    runNpmCommand(
+                        "publish --tag ${npmVersion} --access public --registry=${npmRegistry}",
+                        nodeVersion
+                    )
+                }
             }
         }
     }
 } catch (e) {
-    // If there was an exception thrown, the build failed
-    currentBuild.result = "FAILED"
+    currentBuild.result = 'FAILED'
     throw e
-}
-
-def runYarnCommand(String command, String nodeVersion) {
-    def sourceNodeResult = sourceNode(nodeVersion)
-    def yarnCommand = "yarn ${command}"
-    def cmd = sourceNodeResult ? sourceNodeResult + " && " + yarnCommand : yarnCommand
-    echo "Running: ${cmd}"
-    sh cmd
-}
-
-def runNpmCommand(String command, String nodeVersion) {
-    def sourceNodeResult = sourceNode(nodeVersion)
-    def npmCommand = "npm ${command}"
-    def cmd = sourceNodeResult ? sourceNodeResult + " && " + npmCommand : npmCommand
-    echo "Running: ${cmd}"
-    sh cmd
 }
 
 def runGitCommand(String command) {
     def gitCommand = "git ${command}"
+
     echo "Running: ${gitCommand}"
     sh gitCommand
+}
+
+def runNodeCommand(String command, String nodeVersion) {
+    def sourceNodeResult = sourceNodejs(nodeVersion)
+    def cmd = sourceNodeResult ? sourceNodeResult + ' && ' + command : command
+
+    sh(
+        script: cmd,
+        returnStdout: true
+    ).trim()
+}
+
+def runNpmCommand(String command, String nodeVersion) {
+    def sourceNodeResult = sourceNodejs(nodeVersion)
+    def npmCommand = "npm ${command}"
+    def cmd = sourceNodeResult ? sourceNodeResult + ' && ' + npmCommand : npmCommand
+
+    echo "Running: ${cmd}"
+    sh cmd
+}
+
+def runYarnCommand(String command, String nodeVersion) {
+    def sourceNodeResult = sourceNodejs(nodeVersion)
+    def yarnCommand = "yarn ${command}"
+    def cmd = sourceNodeResult ? sourceNodeResult + ' && ' + yarnCommand : yarnCommand
+
+    echo "Running: ${cmd}"
+    sh cmd
 }
